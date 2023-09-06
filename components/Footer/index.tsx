7e70867fc6478f6780a0423c42e258ae04f60bf1
CamilinLopez <camilollc98@gmail.com>
2023-09-06 07:51:08 -0500

    otro commit

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -30,7 +30,7 @@ const Footer = () => {
                 </Link>
                 <p className="mb-9 text-base font-medium leading-relaxed text-body-color">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Integer lobortis.
+                  Integer lobortis kjnklkml.
                 </p>
                 <div className="flex items-center">
                   <a
