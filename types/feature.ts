9aab6a1f6bed6eac3ae4012858d0bd89d5edefc9
CamilinLopez <camilollc98@gmail.com>
2023-09-07 13:03:49 -0500

    hasta proceso

--- types/feature.ts
+++ types/feature.ts
@@ -1,6 +1,15 @@
+// export type Feature = {
+//   id: number;
+//   icon: JSX.Element;
+//   title: string;
+//   paragraph: string;
+// };
+
+import { type } from "os";
+
 export type Feature = {
   id: number;
-  icon: JSX.Element;
+  week: string;
   title: string;
   paragraph: string;
 };
