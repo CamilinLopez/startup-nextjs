43f7d7c7bd2d2863f04bd0983725bd77a1c986ca
CamilinLopez <camilollc98@gmail.com>
2023-09-08 16:07:52 -0500

    brands names

--- components/Brands/index.tsx
+++ components/Brands/index.tsx
@@ -4,61 +4,61 @@ import Image from "next/image";
 const brandsData: Brand[] = [
   {
     id: 1,
-    name: "mongo",
+    name: "Mongo DB",
     href: "https://www.mongodb.com/es",
     image: "/images/brands/newImages/mongo.svg",
   },
   {
     id: 2,
-    name: "next",
+    name: "Next JS",
     href: "https://nextjs.org/",
     image: "/images/brands/newImages/next.svg",
   },
   {
     id: 3,
-    name: "node",
+    name: "Node JS",
     href: "https://nodejs.org/es",
     image: "/images/brands/newImages/nodejs.svg",
   },
   {
     id: 4,
-    name: "webpay",
+    name: "Webpay",
     href: "https://www.webpay.cl",
     image: "/images/brands/newImages/webpay.svg",
   },
   {
     id: 5,
-    name: "supabase",
+    name: "Supabase",
     href: "https://supabase.com/",
     image: "/images/brands/newImages/supabase.svg",
   },
   {
     id: 6,
-    name: "react",
+    name: "React JS",
     href: "https://es.react.dev/",
     image: "/images/brands/newImages/react.svg",
   },
   {
     id: 7,
-    name: "versel",
+    name: "Versel",
     href: "https://vercel.com/",
     image: "/images/brands/newImages/versel.svg",
   },
   {
     id: 8,
-    name: "tailnwind",
+    name: "Tailnwind CCS",
     href: "https://tailwindcss.com/",
     image: "/images/brands/newImages/tailwind.svg",
   },
   {
     id: 9,
-    name: "figma",
+    name: "Figma",
     href: "https://www.figma.com/",
     image: "/images/brands/newImages/figma.svg",
   },
   {
     id: 10,
-    name: "stripe",
+    name: "Stripe",
     href: "https://stripe.com/es-us",
     image: "/images/brands/newImages/stripe.svg",
   },
@@ -71,9 +71,9 @@ const Brands = () => {
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4">
             <div
-              className="wow fadeInUp flex flex-wrap items-center justify-center rounded-md bg-dark py-8 px-8 dark:bg-primary dark:bg-opacity-5 sm:px-10 md:py-[40px] md:px-[50px] xl:p-[50px] 2xl:py-[60px] 2xl:px-[70px]"
+              className="wow fadeInUp flex flex-wrap items-center justify-center rounded-md bg-dark py-8 px-3 dark:bg-primary dark:bg-opacity-5 sm:px-10 md:py-[40px] md:px-[50px] xl:p-[50px] 2xl:py-[60px] 2xl:px-[70px]"
               data-wow-delay=".1s
-              "
+            "
             >
               {brandsData.map((brand) => (
                 <SingleBrand key={brand.id} brand={brand} />
@@ -92,7 +92,7 @@ const SingleBrand = ({ brand }: { brand: Brand }) => {
   const { href, image, name } = brand;
 
   return (
-    <div className="mx-3 flex w-full max-w-[160px] items-center justify-center py-[15px] sm:mx-4 lg:max-w-[130px] xl:mx-6 xl:max-w-[150px] 2xl:mx-8 2xl:max-w-[160px]">
+    <div className="mx-3 flex w-full max-w-[160px] flex-col items-center justify-center py-[15px] sm:mx-4 lg:max-w-[130px] xl:mx-6 xl:max-w-[150px] 2xl:mx-8 2xl:max-w-[160px]">
       <a
         href={href}
         target="_blank"
@@ -101,6 +101,7 @@ const SingleBrand = ({ brand }: { brand: Brand }) => {
       >
         <Image src={image} alt={name} fill />
       </a>
+      <p>{name}</p>
     </div>
   );
 };
