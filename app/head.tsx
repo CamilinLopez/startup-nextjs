bc91f742e955d3e1339763e115f11d6da0fda99e
CamilinLopez <camilollc98@gmail.com>
2023-10-26 10:22:32 -0500

    cambio logo head

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>Creamos productos mínimos viables digitales</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="ayudamos a fundadores a materializar sus ideas" />
-      <link rel="icon" href="/images/favicon.ico" />
+      <link rel="icon" href="/images/logo/newlogo/PROTOLYr 4.jpg" />
     </>
   );
 }
