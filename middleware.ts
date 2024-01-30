15a396577a9e4a61cd82c47e2b19ab0a669421ed
CamilinLopez <camilollc98@gmail.com>
2024-01-30 12:02:00 -0500

    prueba cookies 2

--- middleware.ts
+++ middleware.ts
@@ -1,39 +1,42 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextApiResponse, NextApiRequest } from "next";
+import cookies from "next-cookies";
 
-export const middleware = async (request: NextRequest) => {
-  const { cookies } = request;
+export const middleware = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { userid } = cookies({ req });
 
-  if (request.nextUrl.pathname.startsWith("/dashboard")) {
-    cookies.set("infocami", "1998");
-    return NextResponse.redirect(new URL("https://www.protolylab.digital"));
-    // if (!token || !id) {
-    //   return NextResponse.redirect(new URL("https://www.protolylab.digital"));
-    // }
+  console.log(userid);
 
-    // try {
-    //   const response = await fetch("https://protolylab.onrender.com/verify", {
-    //     headers: {
-    //       Authorization: `${token}`,
-    //     },
-    //   });
-    //   const data = await response.json();
-    //   console.log(data);
+  // if (request.nextUrl.pathname.startsWith("/dashboard")) {
+  //   cookies.set("infocami", "1998");
+  //   return NextResponse.redirect(new URL("https://www.protolylab.digital"));
+  // if (!token || !id) {
+  //   return NextResponse.redirect(new URL("https://www.protolylab.digital"));
+  // }
 
-    //   const dataUser = await fetch(
-    //     `https://protolylab.onrender.com/user?id=${id}`,
-    //     {
-    //       method: "GET",
-    //     }
-    //   );
+  // try {
+  //   const response = await fetch("https://protolylab.onrender.com/verify", {
+  //     headers: {
+  //       Authorization: `${token}`,
+  //     },
+  //   });
+  //   const data = await response.json();
+  //   console.log(data);
 
-    //   const data1 = await dataUser.json();
+  //   const dataUser = await fetch(
+  //     `https://protolylab.onrender.com/user?id=${id}`,
+  //     {
+  //       method: "GET",
+  //     }
+  //   );
 
-    //   if (!data.autenticado || !data1.isadmin)
-    //     return NextResponse.redirect(new URL("https://www.protolylab.digital"));
-    // } catch (error) {
-    //   console.log(error);
-    // }
-  }
+  //   const data1 = await dataUser.json();
+
+  //   if (!data.autenticado || !data1.isadmin)
+  //     return NextResponse.redirect(new URL("https://www.protolylab.digital"));
+  // } catch (error) {
+  //   console.log(error);
+  // }
+  // }
 
   return;
 };
