8e8afa00936340f14e2c6964d3e7dd8aaf2bd4fa
CamilinLopez <camilollc98@gmail.com>
2024-01-31 11:15:12 -0500

    fix cookie 1

--- middleware.ts
+++ middleware.ts
@@ -1,41 +1,51 @@
 import { NextRequest, NextResponse } from "next/server";
+import type { NextApiRequest, NextApiResponse } from "next";
+
 import { cookies } from "next/headers";
 
-export const middleware = async (request: NextRequest) => {
-  const cookie = cookies();
+export const middleware = async (req: NextApiRequest, request: NextRequest) => {
+  const viewedWelcomeMessage = req.cookies.userid;
+
   if (request.nextUrl.pathname.startsWith("/dashboard")) {
-    const data = cookie.get("userid");
     return NextResponse.redirect(
-      new URL(`https://www.protolylab.digital?id=${data}`)
+      new URL(`https://www.protolylab.digital?id=${viewedWelcomeMessage}`)
     );
-    // if (!token || !id) {
-    //   return NextResponse.redirect(new URL("https://www.protolylab.digital"));
-    // }
-
-    // try {
-    //   const response = await fetch("https://protolylab.onrender.com/verify", {
-    //     headers: {
-    //       Authorization: `${token}`,
-    //     },
-    //   });
-    //   const data = await response.json();
-    //   console.log(data);
-
-    //   const dataUser = await fetch(
-    //     `https://protolylab.onrender.com/user?id=${id}`,
-    //     {
-    //       method: "GET",
-    //     }
-    //   );
-
-    //   const data1 = await dataUser.json();
-
-    //   if (!data.autenticado || !data1.isadmin)
-    //     return NextResponse.redirect(new URL("https://www.protolylab.digital"));
-    // } catch (error) {
-    //   console.log(error);
-    // }
   }
+
+  // const cookie = cookies();
+  // if (request.nextUrl.pathname.startsWith("/dashboard")) {
+  //   const data = cookie.get("userid");
+  //   return NextResponse.redirect(
+  //     new URL(`https://www.protolylab.digital?id=${data}`)
+  //   );
+  // if (!token || !id) {
+  //   return NextResponse.redirect(new URL("https://www.protolylab.digital"));
+  // }
+
+  // try {
+  //   const response = await fetch("https://protolylab.onrender.com/verify", {
+  //     headers: {
+  //       Authorization: `${token}`,
+  //     },
+  //   });
+  //   const data = await response.json();
+  //   console.log(data);
+
+  //   const dataUser = await fetch(
+  //     `https://protolylab.onrender.com/user?id=${id}`,
+  //     {
+  //       method: "GET",
+  //     }
+  //   );
+
+  //   const data1 = await dataUser.json();
+
+  //   if (!data.autenticado || !data1.isadmin)
+  //     return NextResponse.redirect(new URL("https://www.protolylab.digital"));
+  // } catch (error) {
+  //   console.log(error);
+  // }
+  //}
 
   return;
 };
