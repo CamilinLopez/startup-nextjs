6ee67617ffc9b0904b5a866e4833dd939b23152e
CamilinLopez <camilollc98@gmail.com>
2024-01-31 13:06:53 -0500

    fix cookie 7

--- middleware.ts
+++ middleware.ts
@@ -6,10 +6,11 @@ export const middleware = (request: NextRequest) => {
   response.cookies.set("camilo", "1999");
 
   if (request.nextUrl.pathname.startsWith("/dashboard")) {
+    const data = request.cookies.get("camilo");
+    const data1 = request.cookies.get("userid");
+    const data2 = `${data}` + `${data1}`;
     return NextResponse.redirect(
-      new URL(
-        `https://www.protolylab.digital?camilo=${request.cookies.get("camilo")}`
-      )
+      new URL(`https://www.protolylab.digital?camilo=${data2}`)
     );
   }
 
