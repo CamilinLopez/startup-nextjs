d092b9d1877a86018c4eee56760f69db52dc3eb1
CamilinLopez <camilollc98@gmail.com>
2024-02-03 21:21:04 -0500

    finished client production 2

--- config/axiosConfig.ts
+++ config/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios";
 const BACK_PRODUCTION_URL = process.env.BACK_PRODUCTION_URL;
 
 const AxiosInstance: AxiosInstance = axios.create({
-  baseURL: BACK_PRODUCTION_URL,
+  baseURL: "https://protolylab.onrender.com",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
