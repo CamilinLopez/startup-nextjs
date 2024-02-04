4bfbce82f88611f1ffa42edb33ca9fb39f7f58c4
CamilinLopez <camilollc98@gmail.com>
2024-02-03 21:17:33 -0500

    finished client production 1

--- config/axiosConfig.ts
+++ config/axiosConfig.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosInstance } from "axios";
-const BACK_URL = process.env.BACK_PRODUCTION_URL;
+const BACK_PRODUCTION_URL = process.env.BACK_PRODUCTION_URL;
 
 const AxiosInstance: AxiosInstance = axios.create({
-  baseURL: BACK_URL,
+  baseURL: BACK_PRODUCTION_URL,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
