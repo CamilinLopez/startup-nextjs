517a26c30191396f00b63f8bbd172daa54c90460
CamilinLopez <camilollc98@gmail.com>
2024-02-10 13:27:50 -0500

    solution cookies 5

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,13 @@
 "use client";
+import { useRouter } from "next/router";
+
+export const addquery = () => {
+  const router = useRouter();
+  console.log(router.query);
+};
 
 export default function Dashboard() {
-
+  addquery();
   return (
     <>
       <section id="dashboard" className="">
